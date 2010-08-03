bd258151685d5e0084da87c3f0cac002aee4f338
moy.arhiv@gmail.com <moy.arhiv@gmail.com@acdc41da-6259-11de-97f9-b393862d04d8>
2010-08-03 20:00:58 +0000

    initial import

---
+++ trunk/AxleFreeClock/src/name/pilgr/axlefreeclock/AxleFreelock.java
@@ -0,0 +1,13 @@
+package name.pilgr.axlefreeclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AxleFreelock extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
